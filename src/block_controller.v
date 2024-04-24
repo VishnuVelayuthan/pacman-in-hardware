4840df9b604cc5450facd6595803198fae022cef
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2024-04-24 16:09:20 -0700

    Added the functionality from pm controller and view in block controller

--- src/block_controller.v
+++ src/block_controller.v
@@ -2,71 +2,82 @@
 
 module block_controller(
 	input clk, //this clock must be a slow enough clock to view the changing positions of the objects
+	input mastClk,
 	input bright,
 	input rst,
 	input up, input down, input left, input right,
 	input [9:0] hCount, vCount,
 	output reg [11:0] rgb,
-	output reg [11:0] background
+	output reg [11:0] background,	
    );
+
 	wire block_fill;
-	
+	wire maze_fill;
+	wire pellet_fill;
+
+	//legal left and right moves
+   
+
+	reg moveleft;
+	wire [11:0] maze_color;
 	//these two values dictate the center of the block, incrementing and decrementing them leads the block to move in certain directions
-	reg [9:0] xpos, ypos;
-	
+	reg [9:0] pm_xpos, pm_ypos;
+    reg [3:0] pm_direction;
+	wire [11:0] pm_color;
+	wire pm_fill;
+
+	wire leg_l;
+	wire leg_r;
+	wire leg_u;
+	wire leg_d;
+
 	parameter RED   = 12'b1111_0000_0000;
 	
-	/*when outputting the rgb value in an always block like this, make sure to include the if(~bright) statement, as this ensures the monitor 
-	will output some data to every pixel and not just the images you are trying to display*/
+
+	localparam starting_hC = 150;	
+	localparam starting_vC = 34;
+
+	localparam ending_hC = 630;
+	localparam ending_vC = 514;
+
+	//maze_with_color_rom dd_maze(.clk(mastClk),.row(vCount),.col(hCount),.color_data(mazeColor));
+	
+	maze_view dd_maze_view(.p_row(vCount - starting_vC), .p_col(hCount - starting_hC), .color_data(maze_color));
+
+	//pass in xpos and ypos to legal checker
+	//legal checker asks for up, down, left and right, and checks 4bit binary legal moves
+    
+    pacman_controller pm_cont(
+        .clk(clk), .rst(rst), 
+        .l_button(left), .r_button(right), .u_button(up), .d_button(down),
+        .pm_xpos(pm_xpos), .pm_ypos(pm_ypos), .pm_direction(pm_direction)
+    );
+
+    pacman_view pm_view(
+        .clk(clk), .rst(rst),
+        .vC(vCount), .hC(hCount), 
+        .pm_xpos(pm_xpos), .pm_ypos(pm_ypos), .pm_direction(pm_direction),
+        .pac_fill(pm_fill), .color_data(pm_color)
+    );
+    
 	always@ (*) begin
     	if(~bright )	//force black if not inside the display area
 			rgb = 12'b0000_0000_0000;
-		else if (block_fill) 
-			rgb = RED; 
-		else	
-			rgb=background;
+			//edge case: if black, fill rgb with background.
+			//if color is inbetween yellow and black (first three bits are not 111
+			//then also paint background
+		else if (pm_fill && pm_color !=12'b000000000000 && pm_color[11:9]==3'b111) 
+			rgb = pm_color; 
+		else if	(maze_fill)
+			rgb=maze_color;
+		else
+		  rgb=background;
 	end
-		//the +-5 for the positions give the dimension of the block (i.e. it will be 10x10 pixels)
-	assign block_fill=vCount>=(ypos-5) && vCount<=(ypos+5) && hCount>=(xpos-5) && hCount<=(xpos+5);
-	
-	always@(posedge clk, posedge rst) 
-	begin
-		if(rst)
-		begin 
-			//rough values for center of screen
-			xpos<=450;
-			ypos<=250;
-		end
-		else if (clk) begin
-		
-		/* Note that the top left of the screen does NOT correlate to vCount=0 and hCount=0. The display_controller.v file has the 
-			synchronizing pulses for both the horizontal sync and the vertical sync begin at vcount=0 and hcount=0. Recall that after 
-			the length of the pulse, there is also a short period called the back porch before the display area begins. So effectively, 
-			the top left corner corresponds to (hcount,vcount)~(144,35). Which means with a 640x480 resolution, the bottom right corner 
-			corresponds to ~(783,515).  
-		*/
-			if(right) begin
-				xpos<=xpos+2; //change the amount you increment to make the speed faster 
-				if(xpos==800) //these are rough values to attempt looping around, you can fine-tune them to make it more accurate- refer to the block comment above
-					xpos<=150;
-			end
-			else if(left) begin
-				xpos<=xpos-2;
-				if(xpos==150)
-					xpos<=800;
-			end
-			else if(up) begin
-				ypos<=ypos-2;
-				if(ypos==34)
-					ypos<=514;
-			end
-			else if(down) begin
-				ypos<=ypos+2;
-				if(ypos==514)
-					ypos<=34;
-			end
-		end
-	end
+
+		//pacman fill is 30 pixels
+    
+	//mazefill is 264 height by 240 width
+	assign maze_fill = (hCount>=starting_hC && hCount<=ending_hC &&vCount>=starting_vC && vCount<=ending_vC);
 	
 	//the background color reflects the most recent button press
 	always@(posedge clk, posedge rst) begin
